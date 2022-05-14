38012d6bce0071876296f61da967f519801ccc6a
dirkf <fieldhouse@gmx.net>
2022-05-14 18:48:49 +0100

    [TNAFlix] Overhaul extractor module
    * fix EMPFlixIE
    * fix category extraction
    * detect webpage error
    * canonicalise thumbnail URLs
    * return uploader_id, not uploader
    * fix tests

--- youtube_dl/extractor/tnaflix.py
+++ youtube_dl/extractor/tnaflix.py
@@ -5,12 +5,22 @@ import re
 from .common import InfoExtractor
 from ..compat import compat_str
 from ..utils import (
+    clean_html,
+    dict_get,
+    ExtractorError,
     fix_xml_ampersands,
     float_or_none,
+    get_element_by_class,
+    get_element_by_id,
+    get_elements_by_class,
     int_or_none,
+    merge_dicts,
+    parse_count,
     parse_duration,
     str_to_int,
+    strip_or_none,
     unescapeHTML,
+    url_or_none,
     xpath_text,
 )
 
@@ -22,7 +32,7 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
         r'<input[^>]+name="config\d?" value="(?P<url>[^"]+)"',
         r'config\s*=\s*(["\'])(?P<url>(?:https?:)?//(?:(?!\1).)+)\1',
     ]
-    _HOST = 'tna'
+    _HOST = 'tnaflix'
     _VKEY_SUFFIX = ''
     _TITLE_REGEX = r'<input[^>]+name="title" value="([^"]+)"'
     _DESCRIPTION_REGEX = r'<input[^>]+name="description" value="([^"]+)"'
@@ -67,12 +77,33 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
         height = int_or_none(xpath_text(timeline, './imageHeight', 'thumbnail height'))
 
         return [{
-            'url': self._proto_relative_url(pattern_el.text.replace('#', compat_str(i)), 'http:'),
+            'url': url,
             'width': width,
             'height': height,
-        } for i in range(first, last + 1)]
+        } for url in (url_or_none(self._proto_relative_url(pattern_el.text.replace('#', compat_str(i)), 'http:'))
+                      for i in range(first, last + 1))
+            if url]
 
-    def _real_extract(self, url):
+    def _extract_cat_elements(self, html):
+        """Return list of any elements containing categories and tags"""
+
+        if self._CATEGORIES_REGEX is None or not html:
+            return None
+        return get_elements_by_class(
+            'video-link-detailed',
+            self._search_regex(self._CATEGORIES_REGEX, html, 'categories', default=''))
+
+    def _extract_cats(self, html):
+        """Return lists of categories and tags, if found"""
+
+        cats = self._extract_cat_elements(html)
+        if not cats:
+            return None, None
+        # the sites have 'Category' and 'tag'
+        cats = ((x, None) if x[:1].isupper() else (None, x) for x in map(clean_html, cats or []) if x)
+        return (sorted(filter(None, x)) for x in zip(*cats))
+
+    def _real_extract(self, url, webpage=None):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id')
         for display_id_key in ('display_id', 'display_id_2'):
@@ -83,7 +114,12 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
         else:
             display_id = video_id
 
-        webpage = self._download_webpage(url, display_id)
+        webpage = self._download_webpage(url, display_id) if webpage is None else webpage
+
+        err = get_element_by_id('error', webpage)
+        if err:
+            raise ExtractorError('%s said: %s' % (
+                self.IE_NAME, clean_html(err) or 'error', ), expected=True)
 
         cfg_url = self._proto_relative_url(self._html_search_regex(
             self._CONFIG_REGEX, webpage, 'flashvars.config', default=None,
@@ -91,7 +127,7 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
 
         if not cfg_url:
             inputs = self._hidden_inputs(webpage)
-            cfg_url = ('https://cdn-fck.%sflix.com/%sflix/%s%s.fid?key=%s&VID=%s&premium=1&vip=1&alpha'
+            cfg_url = ('https://cdn-fck.%s.com/%s/%s%s.fid?key=%s&VID=%s&premium=1&vip=1&alpha'
                        % (self._HOST, self._HOST, inputs['vkey'], self._VKEY_SUFFIX, inputs['nkey'], video_id))
 
         cfg_xml = self._download_xml(
@@ -127,8 +163,8 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
 
         self._sort_formats(formats)
 
-        thumbnail = self._proto_relative_url(
-            xpath_text(cfg_xml, './startThumb', 'thumbnail'), 'http:')
+        thumbnail = url_or_none(self._proto_relative_url(
+            xpath_text(cfg_xml, './startThumb', 'thumbnail'), 'http:'))
         thumbnails = self._extract_thumbnails(cfg_xml)
 
         title = None
@@ -152,8 +188,7 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
         comment_count = str_to_int(extract_field(self._COMMENT_COUNT_REGEX, 'comment count'))
         average_rating = float_or_none(extract_field(self._AVERAGE_RATING_REGEX, 'average rating'))
 
-        categories_str = extract_field(self._CATEGORIES_REGEX, 'categories')
-        categories = [c.strip() for c in categories_str.split(',')] if categories_str is not None else []
+        categories, tags = self._extract_cats(webpage)
 
         return {
             'id': video_id,
@@ -164,11 +199,12 @@ class TNAFlixNetworkBaseIE(InfoExtractor):
             'thumbnails': thumbnails,
             'duration': duration,
             'age_limit': age_limit,
-            'uploader': uploader,
+            'uploader_id': uploader,
             'view_count': view_count,
             'comment_count': comment_count,
             'average_rating': average_rating,
             'categories': categories,
+            'tags': tags,
             'formats': formats,
         }
 
@@ -177,6 +213,7 @@ class TNAFlixNetworkEmbedIE(TNAFlixNetworkBaseIE):
     _VALID_URL = r'https?://player\.(?:tna|emp)flix\.com/video/(?P<id>\d+)'
 
     _TITLE_REGEX = r'<title>([^<]+)</title>'
+    _CATEGORIES_REGEX = None
 
     _TESTS = [{
         'url': 'https://player.tnaflix.com/video/6538',
@@ -213,6 +250,7 @@ class TNAFlixIE(TNAEMPFlixBaseIE):
     _VALID_URL = r'https?://(?:www\.)?tnaflix\.com/[^/]+/(?P<display_id>[^/]+)/video(?P<id>\d+)'
 
     _TITLE_REGEX = r'<title>(.+?) - (?:TNAFlix Porn Videos|TNAFlix\.com)</title>'
+    _CATEGORIES_REGEX = r'''(?s)<div\b[^>]+?class\s*=\s*['"]_video_info[^\w-].*?</a>(.+?)</div\b'''
 
     _TESTS = [{
         # anonymous uploader, no categories
@@ -231,17 +269,17 @@ class TNAFlixIE(TNAEMPFlixBaseIE):
     }, {
         # non-anonymous uploader, categories
         'url': 'https://www.tnaflix.com/teen-porn/Educational-xxx-video/video6538',
-        'md5': '0f5d4d490dbfd117b8607054248a07c0',
+        'md5': 'add5a9fa7f4da53d3e9d0845ac58f20c',
         'info_dict': {
             'id': '6538',
             'display_id': 'Educational-xxx-video',
             'ext': 'mp4',
-            'title': 'Educational xxx video',
+            'title': 'Educational xxx video (G Spot)',
             'description': 'md5:b4fab8f88a8621c8fabd361a173fe5b8',
             'thumbnail': r're:https?://.*\.jpg$',
             'duration': 164,
             'age_limit': 18,
-            'uploader': 'bobwhite39',
+            'uploader_id': 'bobwhite39',
             'categories': list,
         }
     }, {
@@ -253,24 +291,27 @@ class TNAFlixIE(TNAEMPFlixBaseIE):
 class EMPFlixIE(TNAEMPFlixBaseIE):
     _VALID_URL = r'https?://(?:www\.)?empflix\.com/(?:videos/(?P<display_id>.+?)-|[^/]+/(?P<display_id_2>[^/]+)/video)(?P<id>[0-9]+)'
 
-    _HOST = 'emp'
+    _HOST = 'empflix'
     _VKEY_SUFFIX = '-1'
-
+    _CATEGORIES_REGEX = r'''(?s)<div\b[^>]*?\bclass\s*=\s*["'](?:[\w-]*\s+)*?video-detail-badges[^\w-][^>]*>(.*?)</div\b'''
     _TESTS = [{
         'url': 'http://www.empflix.com/videos/Amateur-Finger-Fuck-33051.html',
-        'md5': 'bc30d48b91a7179448a0bda465114676',
+        'md5': 'd761c7b26601bd14476cd9512f2654fc',
         'info_dict': {
             'id': '33051',
             'display_id': 'Amateur-Finger-Fuck',
             'ext': 'mp4',
             'title': 'Amateur Finger Fuck',
+            'timestamp': 1233151789,
+            'upload_date': '20090128',
             'description': 'Amateur solo finger fucking.',
             'thumbnail': r're:https?://.*\.jpg$',
             'duration': 83,
             'age_limit': 18,
-            'uploader': 'cwbike',
-            'categories': ['Amateur', 'Anal', 'Fisting', 'Home made', 'Solo'],
-        }
+            'uploader': 'Charley',
+            'uploader_id': 'cwbike',
+            'categories': list,
+        },
     }, {
         'url': 'http://www.empflix.com/videos/[AROMA][ARMD-718]-Aoi-Yoshino-Sawa-25826.html',
         'only_matching': True,
@@ -279,10 +320,50 @@ class EMPFlixIE(TNAEMPFlixBaseIE):
         'only_matching': True,
     }]
 
+    def _real_extract(self, url):
+        mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group('id')
+        display_id = dict_get(mobj.groupdict(), ('display_id', 'display_id_2'))
+        if display_id is None:
+            display_id = video_id
+
+        webpage = self._download_webpage(url, display_id)
+
+        info = self._search_json_ld(webpage, video_id, default={})
+        if info:
+            player = self._download_json('https://www.empflix.com/ajax/video-player/' + video_id, display_id)
+            if player.get('success'):
+                info = merge_dicts(
+                    {
+                        'id': video_id,
+                        'display_id': display_id,
+                        'age_limit': 18,
+                    },
+                    info)
+
+                badges = self._search_regex(self._CATEGORIES_REGEX, webpage, 'badges', default='')
+                uploader = get_element_by_class('badge-video-info', badges)
+                info['uploader'] = strip_or_none(uploader)
+                info['uploader_id'] = self._search_regex(r'/profile/(\w+)', badges, 'profile', default=None)
+                badges = get_elements_by_class('badge badge-video', badges)
+                info['categories'] = sorted([x.strip() for x in badges if (re.match(r'^\s*[A-Z]', x) and x != uploader)])
+                info['tags'] = sorted([x.strip() for x in badges if re.match(r'^\s*[a-z]', x)])
+                info['view_count'] = next((parse_count(clean_html(x)) for x in badges if 'icon-eye' in x), None)
+
+                info = merge_dicts(
+                    info,
+                    self._parse_html5_media_entries(url, player.get('html', ''), video_id)[0])
+                self._sort_formats(info.get('formats', []))
+                return info
+
+        # in case old formats exist
+        return super(EMPFlixIE, self)._real_extract(url, webpage)
+
 
 class MovieFapIE(TNAFlixNetworkBaseIE):
     _VALID_URL = r'https?://(?:www\.)?moviefap\.com/videos/(?P<id>[0-9a-f]+)/(?P<display_id>[^/]+)\.html'
 
+    _HOST = 'moviefap'
     _VIEW_COUNT_REGEX = r'<br>Views\s*<strong>([\d,.]+)</strong>'
     _COMMENT_COUNT_REGEX = r'<span[^>]+id="comCount"[^>]*>([\d,.]+)</span>'
     _AVERAGE_RATING_REGEX = r'Current Rating\s*<br>\s*<strong>([\d.]+)</strong>'
@@ -300,11 +381,11 @@ class MovieFapIE(TNAFlixNetworkBaseIE):
             'description': 'Experienced MILF giving an Amazing Handjob',
             'thumbnail': r're:https?://.*\.jpg$',
             'age_limit': 18,
-            'uploader': 'darvinfred06',
+            'uploader_id': 'darvinfred06',
             'view_count': int,
             'comment_count': int,
             'average_rating': float,
-            'categories': ['Amateur', 'Masturbation', 'Mature', 'Flashing'],
+            'categories': ['Amateur', 'Flashing', 'Masturbation', 'Mature'],
         }
     }, {
         # quirky single-format case where the extension is given as fid, but the video is really an flv
@@ -318,10 +399,17 @@ class MovieFapIE(TNAFlixNetworkBaseIE):
             'description': 'Amateur',
             'thumbnail': r're:https?://.*\.jpg$',
             'age_limit': 18,
-            'uploader': 'whiskeyjar',
+            'uploader_id': 'whiskeyjar',
             'view_count': int,
             'comment_count': int,
             'average_rating': float,
             'categories': ['Amateur', 'Teen'],
-        }
+        },
+        # 'skip': 'This video does not exist.',
     }]
+
+    def _extract_cat_elements(self, html):
+
+        return re.findall(
+            r'(?s)<a\b[^>]*>(.*?)</a\b',
+            self._search_regex(self._CATEGORIES_REGEX, html, 'categories', default=''))
